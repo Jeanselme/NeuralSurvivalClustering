9d0e5b55aa5573d934301cbf5ef599680a5d21c2
Vincent Jeanselme <vincent.jeanselme@gmail.com>
2022-10-26 10:36:11 +0100

    Update computation of derivative

--- nsc/nsc_torch.py
+++ nsc/nsc_torch.py
@@ -124,7 +124,7 @@ class NeuralSurvivalClusterTorch(nn.Module):
       if gradient:
         int = []
         for risk in range(self.risks):
-          int.append(grad(outcome[:, risk].mean(), tau_outcome, create_graph = True)[0].unsqueeze(1))
+          int.append(grad(outcome[:, risk].sum(), tau_outcome, create_graph = True)[0].unsqueeze(1))
         int = torch.cat(int, -1).unsqueeze(-1)
         intensity.append(int)
 
